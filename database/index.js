3a5fcf270e1cc71762a429fef576e26faadd5bdc
Philip Hamner <philiphamner@gmail.com>
2020-06-06 22:37:23 -0700

    code cleanup.

--- database/index.js
+++ database/index.js
@@ -68,7 +68,6 @@ db.once('open', function() {
   });
   var Drink = mongoose.model('Drink', drinkSchema);
 
-
   const populateArrayWithDrinks = function(){
     for (var i = 0; i < drinks.length; i++) {
       let drinkID = drinks[i].idDrink;
@@ -77,11 +76,10 @@ db.once('open', function() {
         // console.error('error:', error); // Print the error if one occurred
         // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         // arrayOfDrinks.push(body); // Print the HTML for the Google homepage.
-        // console.log(body)
+
         let json = JSON.parse(body);
         let drinkObj = json.drinks[0]
         var currentDrink = new Drink(drinkObj);
-        // console.log(json)
 
         currentDrink.save(function (err, currentDrink) {
           if (err){
@@ -91,60 +89,8 @@ db.once('open', function() {
             console.log(`success in logging ${drinkID} to mongo`)
           }
         });
-
-
-        // arrayOfDrinks.push(json.drinks[0])
-        // console.log(json.drinks[0])
       });
     }
   }
   populateArrayWithDrinks();
-
-
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Grabbing drink data from API:
-// let arrayOfDrinks = [];
-// const populateArrayWithDrinks = function(){
-//   for (var i = 0; i < drinks.length; i++) {
-//     let drinkID = drinks[i].idDrink;
-
-//     request(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkID}`, function (error, response, body) {
-//       // console.error('error:', error); // Print the error if one occurred
-//       // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//       // arrayOfDrinks.push(body); // Print the HTML for the Google homepage.
-//       // console.log(body)
-//       let json = JSON.parse(body);
-//       arrayOfDrinks.push(json.drinks[0])
-//       console.log(json.drinks[0])
-//     });
-//   }
-// }
-
-// //Testing data from API:
-// const checkArrInASec = function(){
-//   setTimeout(function(){ console.log(arrayOfDrinks[0]); }, 10000);
-// }
-
-// populateArrayWithDrinks();
-// checkArrInASec();
