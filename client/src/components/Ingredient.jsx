c47479021f0bce4c6208c3d5812b9993151b7efc
Philip Hamner <philiphamner@gmail.com>
2020-06-07 17:32:23 -0700

    Make ingredient buttons interactive.

--- client/src/components/Ingredient.jsx
+++ client/src/components/Ingredient.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: ${props => props.trueOrFalse ? "palevioletred" : "white"};
+  color: ${props => props.trueOrFalse ? "white" : "palevioletred"};
+
+  font-size: 1em;
+  margin: 3px;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
 
 const Ingredient = function(props){
   return(
     <div>
-      <div onClick={props.addItemToBar} id={props.item}>{props.item}</div>
-      {/* {console.log(props.item)} */}
+      <Button trueOrFalse={props.trueFalse} onClick={props.addItemToBar} id={props.item}>{props.item}</Button>
+      {/* {console.log(props.trueFalse)} */}
     </div>
   )
 }
--- client/src/components/IngredientSelector.jsx
+++ client/src/components/IngredientSelector.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Ingredient from './Ingredient.jsx';
 
 const IngredientSelector = function(props){
-  const arrayOfIngredients = Object.keys(props.incredients);
+  const arrayOfIngredients = Object.keys(props.ingredients);
+  // console.log(props.ingredients.Vodka)
   return(
     <div>
       <h3>Select the Ingredients in your Bar</h3>
@@ -10,8 +11,8 @@ const IngredientSelector = function(props){
         Filter: <input type="text" name="name" />
       </form>
       <br />
-      {console.log(arrayOfIngredients)}
-      {arrayOfIngredients.map(item => <Ingredient item={item} key={item} addItemToBar={props.addItemToBar}/>)}
+      {/* {console.log(arrayOfIngredients)} */}
+      {arrayOfIngredients.map(item => <Ingredient item={item} key={item} trueFalse={props.ingredients[item]} addItemToBar={props.addItemToBar}/>)}
 
     </div>
   )
